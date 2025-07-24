96bcd927b0884f3b880444396b14b2a02fbbad4e
Amr Fadel <afadel1310@gmail.com>
2025-07-24 14:40:06 +0300

    Update store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -35,7 +35,7 @@ const store = configureStore({
     cart,
     wishlist,
   },
-  devTools: import.meta.env.MODE !== "production",
+  devTools: false,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
