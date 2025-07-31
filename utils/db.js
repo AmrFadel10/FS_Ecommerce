70d13e94b110676e837fd00fcdb591ffafa5e79c
Amr Fadel <afadel1310@gmail.com>
2025-07-31 09:24:54 +0300

    some efit for performance

--- utils/db.js
+++ utils/db.js
@@ -5,8 +5,8 @@ exports.DbConnection = async () => {
     await mongoose.connect(process.env.DB_URI, {
       bufferCommands: false,
       maxPoolSize: 10,
-      serverSelectionTimeoutMS: 30000, // 30 seconds
-      socketTimeoutMS: 45000, // 45 seconds
+      serverSelectionTimeoutMS: 30000,
+      socketTimeoutMS: 45000,
     });
     console.log("The database connected");
   } catch (error) {
